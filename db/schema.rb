3e8894d93bd776ca33bca0d83c71e135f50bf7b6
ichisei <132669382+ichisei@users.noreply.github.com>
2023-06-06 05:12:28 +0000

    [Finish]o-kadai3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_05_091223) do
+ActiveRecord::Schema.define(version: 2023_06_06_012025) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -67,6 +67,11 @@ ActiveRecord::Schema.define(version: 2023_06_05_091223) do
     t.text "comment"
     t.integer "user_id"
     t.integer "book_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
