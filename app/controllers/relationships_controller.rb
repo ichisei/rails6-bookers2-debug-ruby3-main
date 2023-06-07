8b212112d951703a3341543c3b893d43e7ac502e
ichisei <132669382+ichisei@users.noreply.github.com>
2023-06-07 10:29:34 +0000

    [Add]o-kadai4

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,45 @@
+class RelationshipsController < ApplicationController
+  
+  def create
+    current_user.follow(params[:user_id])
+    redirect_to request.referer
+  end
+
+  def destroy
+    current_user.unfollow(params[:user_id])
+    redirect_to request.referer  
+  end
+
+  def followings
+    user = User.find(params[:user_id])
+    @users = user.followings
+  end
+  
+  def followers
+    user = User.find(params[:user_id])
+    @users = user.followers
+  end
+
+end
+  
+  
+  
+# def create
+#     user = User.find(params[:user_id])
+#     relationships = Relationship.new(followed_id: user_id)
+#     relationships.followerd_id = current_user.id
+#     relationships.save
+#     redirect_to request.referer
+#   end
+
+#   def destroy
+#     user = User.find(params[:id])
+#     relationship = favorites.find_by(followed_id: current_user_id)
+#     relationship.destroy
+#     redirect_to request.referer
+#   end  
+  
+  
+  
+  
+  
